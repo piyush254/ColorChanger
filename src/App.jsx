b91d3c75e7fb099de484614ee51313247f4d248e
piyush254 <76161817+piyush254@users.noreply.github.com>
2024-03-17 07:53:46 +0530

    commit it

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,23 @@ import React, { useState } from "react";
 import ColorButton from "./Component/ColorButton";
 
 function App() {
-  const [color, setColor] = useState("khaki");
+  const [colour, setColor] = useState("#a986ed");
 
   return (
     <div
       className="w-full h-screen duration-200"
-      style={{ backgroundColor: color }}
+      style={{ backgroundColor: colour }}
     >
       <div className="fixed flex flex-wrap justify-center  bottom-12 inset-x-0 px-2">
         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2  rounded-3xl">
-          <ColorButton setColor={setColor} color="red" />
-          <ColorButton setColor={setColor} color="green" />
-          <ColorButton setColor={setColor} color="blue" />
-          <ColorButton setColor={setColor} color="white" />
-          <ColorButton setColor={setColor} color="pink" />
-          <ColorButton setColor={setColor} color="yellow" />
-          <ColorButton setColor={setColor} color="purple" />
-          <ColorButton setColor={setColor} color="black" />
+          <ColorButton setColor={setColor} colour="red" />
+          <ColorButton setColor={setColor} colour="green" />
+          <ColorButton setColor={setColor} colour="blue" />
+          <ColorButton setColor={setColor} colour="white" />
+          <ColorButton setColor={setColor} colour="pink" />
+          <ColorButton setColor={setColor} colour="yellow" />
+          <ColorButton setColor={setColor} colour="purple" />
+          <ColorButton setColor={setColor} colour="black" />
         </div>
       </div>
     </div>
--- src/Component/ColorButton.jsx
+++ src/Component/ColorButton.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-function ColorButton({ setColor, color="red" }) {
+function ColorButton({ setColor, colour = "red" }) {
   return (
     <button
-      onClick={() => setColor(color)}
+      onClick={() => setColor(colour)}
       className="outline-none px-4 ppy-1 rounded-full text-white shadow-lg"
-      style={{ backgroundColor: color,color: "violet" }}
+      style={{ backgroundColor: colour, color: "violet" }}
     >
-     {color.charAt(0).toUpperCase() + color.slice(1)}
+      {colour.charAt(0).toUpperCase() + colour.slice(1)}
     </button>
   );
 }
